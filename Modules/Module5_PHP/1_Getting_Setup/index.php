7e28880c1d79f1f81c5006837d0422e57720a284
Dustin Woodard <dustin.woodard@gmail.com>
2023-04-18 21:01:56 -0600

    css

--- Modules/Module5_PHP/1_Getting_Setup/index.php
+++ Modules/Module5_PHP/1_Getting_Setup/index.php
@@ -6,14 +6,18 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../css/common.css">
 </head>
 
 <body>
-
-    <h1><?php echo "Hello World <br>"; ?></h1>
-    <!-- test.php link -->
-    <a href="test.php">Test</a>
-
+    <div class="wrapper">
+        <h1><?php echo "Hello World <br>"; ?></h1>
+        <ul>
+            <li>
+                <a href="test.php">Test</a>
+            </li>
+        </ul>
+    </div>
 </body>
 
 </html>
--- Modules/Module5_PHP/index.php
+++ Modules/Module5_PHP/index.php
@@ -6,13 +6,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+
+    <link rel="stylesheet" href="css/common.css">
+
 </head>
 
 <body>
-    <h1>
-        <?php echo "PHP Server is working"; ?>
-    </h1>
+    <div class="wrapper">
 
+
+        <h1>
+            <?php echo "PHP Server is working"; ?>
+        </h1>
+
+        <ul>
+            <li><a href="/1_Getting_Setup/">1_Getting_Setup</a></li>
+        </ul>
+    </div>
 </body>
 
 </html>
