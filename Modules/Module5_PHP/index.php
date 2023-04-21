90244b78d0dd3fd72a80eb17f34f80d911824eac
DauntlessShark <crramesserly@gmail.com>
2023-04-21 13:02:47 -0600

    Merge branch 'green23' of https://github.com/harc-agency/students.storkmentors.com into green23

---
+++ Modules/Module5_PHP/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link rel="stylesheet" href="css/common.css">
+
+</head>
+
+<body>
+    <div class="wrapper">
+
+
+        <h1>
+            <?php echo "PHP Server is working"; ?>
+        </h1>
+
+        <ul>
+            <li><a href="1_getting_setup/">1_Getting_Setup</a></li>
+            <li><a href="2_basic_syntax/">2_Basic_Syntax</a></li>
+            <li><a href="3_Laravel/">3_Laravel</a></li>
+        </ul>
+    </div>
+</body>
+
+</html>
