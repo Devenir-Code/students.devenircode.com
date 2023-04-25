f0cd760c456fff4800b99631bf6a97a1f0ba5db4
Dustin Woodard <dustin.woodard@gmail.com>
2023-04-25 15:04:57 -0600

    oop

--- Modules/Module5_PHP_part1/2_basic_syntax/7_oop.php
+++ Modules/Module5_PHP_part1/2_basic_syntax/7_oop.php
@@ -1,37 +1,92 @@
 <?php
 
 
+// Array
+
+$car = [
+    "make" => "Ford",
+    "model" => "Mustang",
+    "color" => "red",
+    "sound" => "Vroom!"
+];
+
+// $car = [
+//     "make" => "Ford",
+//     "model" => "Fiesta",
+//     "color" => "blue",
+//     "sound" => "Beep!"
+// ];
+
+// $car = [
+//     "make" => "Tesla",
+//     "model" => "Model S",
+//     "color" => "black",
+//     "sound" => "Silence!"
+// ];
+
+// echo the car's make
+
+echo "My car is a " . $car["make"] . " " . $car["model"] . " that is " . $car["color"] . " and I go: " . $car["sound"] . "<br>";
+
+
+
+
+
+echo '<hr>';
+
+
 /* OOP is a way of writing code that uses objects to 
 group related data and behavior, making it 
 easier to write, read, and maintain.*/
 
 // OBJECT ORIENTED PROGRAMMING
-class Car {
+class Car
+{
     public $make;
     public $model;
     public $color;
 
-    public function __construct($make, $model, $color) {
+    public function __construct($make, $model, $color)
+    {
         $this->make = $make;
         $this->model = $model;
         $this->color = $color;
     }
 
-    public function getMake() {
+    public function getMake()
+    {
         return $this->make;
     }
 
-    public function getModel() {
+    public function getModel()
+    {
         return $this->model;
     }
 
-    public function getColor() {
+    public function getColor()
+    {
         return $this->color;
+    }
+
+    // make car sound
+    public function makeSound()
+    {
+
+        if ($this->make == "Ford" && $this->model == "Mustang") {
+            return "Vroom!";
+        } else if ($this->make == "Tesla") {
+            return "Silence!";
+        } else {
+            return "Beep!";
+        }
     }
 }
 
+
+
+
 $myCar = new Car("Ford", "Mustang", "red");
-echo "My car is a " . $myCar->getMake() . " " . $myCar->getModel() . " that is " . $myCar->getColor() . ".<br><br><br>"; // Output: My car is a Ford Mustang that is red.
+echo "My car is a " . $myCar->getMake() . " " . $myCar->getModel() . " that is " . $myCar->getColor() . "And I go: " . $myCar->makeSound() . "<br>";
 
 
 
@@ -39,47 +94,63 @@ echo "My car is a " . $myCar->getMake() . " " . $myCar->getModel() . " that is "
 // In PHP, you can use inheritance to create a class that inherits properties and methods from another class. Here's an example of how to create a three-level inheritance hierarchy with a PHP class starting from `Animal` down to `Elephant`:
 
 
- 
-  class Animal {
+
+class Animal
+{
     public $name;
-    public function __construct($name) {
-      $this->name = $name;
+    public function __construct($name)
+    {
+        $this->name = $name;
     }
-    public function getName() {
-      return $this->name;
+    public function getName()
+    {
+        return $this->name;
     }
-  }
+}
 
-  class Mammal extends Animal {
+class Mammal extends Animal
+{
     public $furColor;
-    public function __construct($name, $furColor) {
-      parent::__construct($name);
-      $this->furColor = $furColor;
+    public function __construct($name, $furColor)
+    {
+        parent::__construct($name);
+        $this->furColor = $furColor;
     }
-    public function getFurColor() {
-      return $this->furColor;
+    public function getFurColor()
+    {
+        return $this->furColor;
     }
-  }
+}
 
-  class Elephant extends Mammal {
+class Elephant extends Mammal
+{
     public $trunkLength;
-    public function __construct($name, $furColor, $trunkLength) {
-      parent::__construct($name, $furColor);
-      $this->trunkLength = $trunkLength;
+    public $canFly;
+
+    public function __construct($name, $furColor, $trunkLength, $canFly = false)
+    {
+        parent::__construct($name, $furColor);
+        $this->trunkLength = $trunkLength;
+        $this->canFly = $canFly;
     }
-    public function getTrunkLength() {
-      return $this->trunkLength;
+
+    public function getTrunkLength()
+    {
+        return $this->trunkLength;
     }
-  }
 
-  $elephant = new Elephant("Dumbo", "Gray", 2);
-  echo "Name: " . $elephant->getName() . "\n";
-  echo "Fur Color: " . $elephant->getFurColor() . "\n";
-  echo "Trunk Length: " . $elephant->getTrunkLength() . "\n";
- 
+    // write a method about elephants flying
+    public function fly()
+    {
+        return "my name is " . $this->name . " and I can fly!";
+    }
+}
 
-// In this example, we have created three classes: `Animal`, `Mammal`, and `Elephant`. The `Mammal` class extends the `Animal` class and inherits its properties and methods. The `Elephant` class extends the `Mammal` class and inherits its properties and methods.
+$elephant = new Elephant("Dumbo", "Gray", 2, false);
 
-// We have also created an instance of the `Elephant` class named `$elephant` and assigned it the values `"Dumbo"`, `"Gray"`, and `2`. We can then use the inherited methods to access the properties of the `$elephant` object.
+echo '<hr>';
 
-// Is there anything else you would like to know about inheritance in PHP?
+echo "Name: " . $elephant->getName() . "<br>";
+echo "Fur Color: " . $elephant->getFurColor() . "<br>";
+echo "Trunk Length: " . $elephant->getTrunkLength() . "<br>";
+echo "Can Fly: " . ($elephant->canFly ? "Yes" : "No") . "<br>";
