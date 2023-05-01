7edcac5581d2b0061598cbe555ad7193f55d581b
Dustin Woodard <dustin.woodard@gmail.com>
2023-05-01 09:04:11 -0600

    update

--- Modules/Module5_PHP_part1/examples/madlib.php
+++ Modules/Module5_PHP_part1/examples/madlib.php
@@ -1,4 +1,6 @@
 <!-- 
+    Instructions:
+
     In this example we are going to create a simple madlib form.
     The user will enter some words and we will use those words to create a madlib story.
 
@@ -7,10 +9,12 @@
     After you submit the form, you will see the values that were submitted in the URL.
     Like: madlib.php?name=John&color=blue&food=pizza&animal=dog&adjective1=funny&adjective2=smelly
 
-    Anything after the ? is called a query string, and you can access the values using the $_GET superglobal.
+    Anything after the ? is called a query string, and you can access the values using
+    the $_GET superglobal.
 
-    We'll use GET to set the values of the form fields if they are set.
-    We'll also use GET to show the madlib story after the form is submitted.
+    example: $_GET['name'] will return the value from the url: madlib.php?name=John
+    
+    We'll also use $_GET to show the madlib story after the form is submitted.
  -->
 
 
@@ -20,8 +24,11 @@
 // Set variables to null if they are not set
 
 
-// this ?? is a null coalescing operator, its a shorthand way of writing the ternary operator:
-// isset($_GET['name'])? $_GET['name'] : null;
+/* 
+Notice:
+this ?? is a null coalescing operator, its a shorthand way of writing the ternary operator:
+isset($_GET['name'])? $_GET['name'] : null;
+*/
 
 $name = $_GET['name'] ?? null;
 $color = $_GET['color'] ?? null;
@@ -31,7 +38,7 @@ $adjective1 = $_GET['adjective1'] ?? null;
 $adjective2 = $_GET['adjective2'] ?? null;
 
 
-//only show the form if the user has not submitted it
+//only show the The madlib if the user has not submitted it
 $showMadlib = false;
 if (
     isset($name) &&
@@ -99,8 +106,12 @@ if (
     <h1> Madlib Example:</h1>
 
     <div class="container">
+        <!-- 
+            Notice: the method="get" attribute on the form tag
+            without the action attribute, the form will submit to the current page
+         -->
         <form method="get">
-            <p>Enter your name:</p>
+            <p>Enter your funny name:</p>
             <input type="text" name="name" value="<?php echo $name ?>">
             <p>Enter an adjective:</p>
             <input type="text" name="adjective1" value="<?php echo $adjective1 ?>">
@@ -116,34 +127,43 @@ if (
             <input type="submit" value="Submit">
         </form>
 
+        <!-- 
+            Instructions:
+            With in each b tag, make it so that the value of the $_GET variable is displayed
 
+            Because we set the vars above we can use them here.
+            For example, the first b tag should look like this:
+            <b><?php echo $name ?></b>
+         -->
         <div id="madlib" <?php if (!$showMadlib) : ?> style="display:none" <?php endif; ?>>
-            Once upon a time, there was a
-            <b><? echo $adjective1 ?></b>
-            person named
-            <b><? echo $name ?></b> .
-            <b><? echo $name ?></b>
-            loved to eat
-            <b> <? echo $adjective2 ?> <? echo $food ?></b>, especially when it was
-            <b><? echo $color ?></b>
-            . One day,
-            <b><? echo $name ?></b>
-            met a
-            <b><? echo $adjective1 ?> <? echo $color ?> <? echo $animal ?></b>
-            in the park. The
-            <b><? echo $animal ?></b>
-            looked hungry, so
-            <b><? echo $name ?> </b> gave it some
-            <b><? echo $food ?></b> .
-            The
-            <b><? echo $animal ?></b>
-            ate the
-            <b><? echo $food ?></b>
-            and thanked
-            <b><? echo $name ?></b>
-            with a big smile.
+            <p>
+                Once upon a time, there was a <b>ADJECTIVE1</b> person named
+                <b>NAME</b>. <b>NAME</b> loved to eat <b>ADJECTIVE2 FOOD</b>,
+                especially when it was <b>COLOR</b>.
+            </p>
+
+            <p>
+                One day, <b>NAME</b> met a <b>ADJECTIVE1 COLOR ANIMAL</b> in the park.
+                The <b>ANIMAL</b> looked hungry, so <b>NAME</b>
+                gave it some <b>FOOD</b>. The <b>ANIMAL</b> ate the <b>FOOD</b>
+                and thanked <b>NAME</b> with a big smile.
+            </p>
         </div>
     </div>
+
+    <div>
+        <!-- 
+        Instructions:    
+        Try using var_dump and print_r to see what is in the $_GET superglobal,
+        within the pre tags 
+    -->
+
+        <pre>
+
+    </pre>
+    </div>
+
+
 
 </body>
 
